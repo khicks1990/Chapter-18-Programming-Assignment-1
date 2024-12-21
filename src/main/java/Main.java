4c5558626bdf5b8e156678d418f6ab52d00c0c9e
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-21 00:51:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // Create a Scanner object to read keyboard input.
+
+
+      // Create a Set to hold strings.
+
+
+      // Read a line of input.
+
+
+      // Tokenize the input.
+
+
+      // Add the tokens to the Set.
+
+
+      // Display the words.
+
+   }
+}
